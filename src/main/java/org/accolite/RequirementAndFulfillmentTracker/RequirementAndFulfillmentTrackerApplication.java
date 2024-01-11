d59ee7012d300d5ba032bcc758419a8cf3d5d044
Kaulwar Mythri <kaulwar.mythri@accolitedigital.com>
2024-01-11 14:50:43 +0530

    initial commit

---
+++ src/main/java/org/accolite/RequirementAndFulfillmentTracker/RequirementAndFulfillmentTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.accolite.RequirementAndFulfillmentTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementAndFulfillmentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementAndFulfillmentTrackerApplication.class, args);
+	}
+
+}
